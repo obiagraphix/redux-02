3142407c23a31f1501c1d10b121c72225a5ae4d8
Sam O-A <yibaah@gmail.com>
2019-12-13 10:48:30 -0500

    plus+ 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import todoApp from "./Store/reducers";
 import "./index.css";
 import App from "./App";
